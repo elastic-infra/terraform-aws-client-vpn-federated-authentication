0ed40fb3b29da990626d26ff4a371bd1de8b13d6
ryotana <dev@saku-lab.net>
2023-07-11 11:59:33 +0000

    aws_ec2_client_vpn_network_association.security_groups has been obsoleted

--- outputs.tf
+++ outputs.tf
@@ -22,8 +22,9 @@ output "vpn_endpoint_security_groups" {
   description = "VPN endpoint security groups"
 
   value = distinct(
-    flatten(
-      [for association in aws_ec2_client_vpn_network_association.this : association.security_groups]
+    concat(
+      [aws_security_group.this.id],
+      var.additional_security_groups,
     )
   )
 }
